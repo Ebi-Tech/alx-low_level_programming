6f3bc778ec8e0b69a68ec3ab3481a2b05296ce33
Ebi-Tech <difebi14@gmail.com>
2022-06-02 13:38:59 +0100

    Puts Not Print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
