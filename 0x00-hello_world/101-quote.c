f4eacc3eff1bae1bcc3b64e004ca93c4cc754865
Ebi-Tech <difebi14@gmail.com>
2022-06-02 15:49:50 +0100

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,12 +8,13 @@
  * Return: 1
  */
 
-
 int main(void)
 
 {
 	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+		"and that piece of art is useful - Dora Korpar, 2015-10-19\n", 
+		s);
 	return (1);
 }
