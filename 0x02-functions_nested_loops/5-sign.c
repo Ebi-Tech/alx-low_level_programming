04dbf1c2c420c9cda72d88ea70cbcf3de44b7bdd
Ebi-Tech <difebi14@gmail.com>
2022-06-07 19:31:30 +0100

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * print_sign - Print sign of number
- * @n: number to check
- * Return:	1 and prints + if n is greater than zero,
+ * print_sign - Print the sign of a number
+ * @n: Character to check
+ * Description: print +, 0, or - depending on number, along with return
+ * Result:	1 and prints + if n is greater than zero
  *		0 and prints 0 if n is zero
  *		-1 and prints - if n is less than zero
  */
