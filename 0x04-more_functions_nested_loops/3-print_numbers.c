6e1d9fe1c5d477b6bd27f2559e711e83acc72cb1
Ebi-Tech <difebi14@gmail.com>
2022-06-09 10:52:03 +0100

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
