351c0f592b8218c4a38129e85572e9423f30a5b9
Ebi-Tech <difebi14@gmail.com>
2022-06-09 12:11:52 +0100

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line
- * Return: 0
+ * Return: void
  */
 
 void print_numbers(void)
@@ -13,6 +13,5 @@ void print_numbers(void)
 	{
 		_putchar(n);
 	}
-	putchar('\n');
-	return (0);
+	_putchar('\n');
 }
