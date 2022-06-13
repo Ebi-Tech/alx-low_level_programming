a975c7190f74e38e7ec858fad90e33a140573488
Ebi-Tech <difebi14@gmail.com>
2022-06-13 11:15:22 +0100

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: character to check
+ */
+
+void print_rev(char *s)
+{
+	int l = 0, i;
+
+	while (s[i++])
+		l++;
+
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
