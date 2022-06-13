4b5a02c23256b2db0b37be2100743e415e4dea16
Ebi-Tech <difebi14@gmail.com>
2022-06-13 10:18:46 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: character to check
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
