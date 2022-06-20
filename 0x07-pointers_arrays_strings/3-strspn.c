11ff70549b7492c9b850fc2705da339c7970cbe1
Ebi-Tech <difebi14@gmail.com>
2022-06-20 12:09:09 +0100

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,7 +7,7 @@
  * Return: Returns the number of bytes in the initial segment
  * of s which consist only of bytes from accept
  */
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int bytes = 0;
 	int index;
