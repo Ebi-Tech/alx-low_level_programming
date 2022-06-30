e05e9bd8efd8fb9163857bebc2d505aae7f00313
Ebi-Tech <difebi14@gmail.com>
2022-06-30 13:30:05 +0100

    101-mul

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -1,18 +1,32 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	char *concat;
+	unsigned long mul;
+	int i, j;
 
-	concat = string_nconcat("Best ", "School !!!", 6);
-	printf("%s\n", concat);
-	free(concat);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98); }
+		}
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
 }
